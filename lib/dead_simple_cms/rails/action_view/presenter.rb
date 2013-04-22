1d628fe86ceeff212a763cfaf713c8ab992c0e4e
Igor Afonov <afonov@gmail.com>
2013-04-22 09:10:52 +0300

    Generate id for group's fieldset

--- lib/dead_simple_cms/rails/action_view/presenter.rb
+++ lib/dead_simple_cms/rails/action_view/presenter.rb
@@ -33,12 +33,13 @@ module DeadSimpleCMS
           JAVASCRIPT
           js << javascript_include_tag(options.delete(:bootstrap_tab_js)) if options[:bootstrap_tab_js]
           lis = DeadSimpleCMS.sections.values.map do |section|
-            content_tag(:li, link_to(section.label, "##{section.identifier.to_s.csserize}", :id => "#{section.identifier.to_s.csserize}-tab"))
+            section_id = Util::String.csserize(section.identifier)
+            content_tag(:li, link_to(section.label, "##{section_id}", :id => "#{section_id}-tab"))
           end.join.html_safe
           tabs = content_tag(:ul, lis, :class => "nav nav-tabs", :id => "section-tabs")
           sections = content_tag(:div, :class => "tab-content") do
             DeadSimpleCMS.sections.values.map do |section|
-              content_tag(:div, section(section), :class => "tab-pane", :id => "#{section.identifier.to_s.csserize}")
+              content_tag(:div, section(section), :class => "tab-pane", :id => Util::String.csserize(section.identifier))
             end.join.html_safe
           end
 
